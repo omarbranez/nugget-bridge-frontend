1767b9e8957430322d164fc5c9a9add1bc7ad915
Omar <omar.branez@gmail.com>
2021-08-23 20:12:43 -0400

    render bottom canvas window background alongside top window

--- src/index.js
+++ src/index.js
@@ -1,50 +1,58 @@
 // const BASE_URL = 'http://localhost:3000'
-// document.addEventListener('DOMContentLoaded', () => {
-
-// gameCanvas is where the game itself will be displayed
 const gameCanvas = document.getElementById("game-overlay")
 const gameContext = gameCanvas.getContext("2d")
-gameCanvas.height = 512
-gameCanvas.width = 888//" //GBA is 1200x800, mine was 1721x800 for SCROLLING //old 400x860
-
-// teamCanvas is where the player's team will be permanently displayed
 const teamCanvas = document.getElementById("team-container")
 const teamContext = teamCanvas.getContext("2d")
-teamCanvas.height = 512
-teamCanvas.width = 888
 
-// change the background depending on the program state
-let currentScreen = "title" // upon load, is title screen
-const screens = ["title", "creation", "login", "options", "overworld", "battle", "result"]
-const background = new Image()
-// background.src = "./assets/title-screen-logo.gif" // need to convert to sprite sheet
-switch (currentScreen) {
-    case "title":
-        background.src = "./assets/title-spritesheet.png"
-        break
-    case "creation":
-        background.src = "./assets/nugget-bridge-creation.png"
-        break
-    case "options":
-        background.src = "./assets/nugget-bridge-options.png"
-        break
-    case "login":
-        background.src = "./assets/nugget-bridge-login.png"
-        break
-    case "overworld":
-        background.src = "./assets/nugget-bridge-overworld.png"
-        break
-    case "battle":
-        background.src = "./assets/nugget-bridge-battle.png"
-        break
-    case "result":
-        background.src = "./assets/nugget-bridge-result.png"
-        break
-    default:
-        background.src = "./assets/title-screen-logo.gif"
+let currentScreen = "title" 
+
+function renderGameWindow() {
+    gameCanvas.height = 512
+    gameCanvas.width = 888//" //GBA is 1200x800, mine was 1721x800 for SCROLLING //old 400x860
+    // const screens = ["title", "creation", "login", "options", "overworld", "battle", "result"]
+    const gameBackground = new Image()
+    switch (currentScreen) {
+        case "title":
+            gameBackground.src = "./assets/title-spritesheet.png"
+            spritesheetAnimate(5, 41, 2220, 10250, gameBackground)
+            break
+        case "initial":
+            gameBackground.src = "./assets/menu-gameBackground.png"
+            break
+        case "creation":
+            gameBackground.src = "./assets/nugget-bridge-creation.png"
+            break
+        case "options":
+            gameBackground.src = "./assets/nugget-bridge-options.png"
+            break
+        case "login":
+            gameBackground.src = "./assets/nugget-bridge-login.png"
+            break
+        case "overworld":
+            gameBackground.src = "./assets/nugget-bridge-overworld.png"
+            break
+        case "battle":
+            gameBackground.src = "./assets/nugget-bridge-battle.png"
+            break
+        case "result":
+            gameBackground.src = "./assets/nugget-bridge-result.png"
+            break
+        default:
+            gameBackground.src = "./assets/title-screen-logo.gif"
+    }
+    console.log("im all the way up!")
 }
-// background.onload = function(){
-//     gameContext.drawImage(background,0,0,860,400) }
+
+function renderTeamWindow() {
+    teamCanvas.height = 512
+    teamCanvas.width = 888
+    const bottomBackground = new Image()
+    bottomBackground.src = "./assets/team-canvas-background.png"
+    bottomBackground.onload = function() { 
+        teamContext.drawImage(bottomBackground, 0, 0)
+    }
+    console.log("started from the bottom")
+}
 
 // controls are keyboard. maybe mouse too?
 gameCanvas.addEventListener("keydown", function(e) {
@@ -53,16 +61,12 @@ gameCanvas.addEventListener("keydown", function(e) {
     }
 }, false);
 
-
-let numColumns = 5
-let numRows = 41
-// Define the size of a frame EACH IS 444 WIDTH 256 HEIGHT for title-spritesheet
-let frameWidth = 2220 / numColumns;; //2220
-let frameHeight = 10250 / numRows;; // 10250
-
-// The sprite image frame starts from 0
-let currentFrame = 0;
-setInterval( function() { // animate spritesheet
+function spritesheetAnimate(numColumns, numRows, sheetWidth, sheetHeight, bgImage) {
+    // const gameContext = gameCanvas.getContext("2d")
+    let frameWidth = sheetWidth / numColumns //2220
+    let frameHeight = sheetHeight / numRows // 10250
+    let currentFrame = 0;
+    setInterval( function() { // animate spritesheet
     currentFrame++ // pick new frame
     let maxFrame = numColumns * numRows - 1
     if (currentFrame > maxFrame){ // loop frames
@@ -70,14 +74,16 @@ setInterval( function() { // animate spritesheet
     }
     let column = currentFrame % numColumns // update rows and columns
     let row = Math.floor(currentFrame / numColumns) // Clear and draw
-    gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-    gameContext.drawImage(background, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 0, 0, gameCanvas.width, gameCanvas.height);
+    // debugger
+    gameContext.drawImage(bgImage, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 0, 0, gameCanvas.width, gameCanvas.height);
     //Wait for next step in the loop
 }, 100);
+}
 
-
-// })
 //check that nothing is hideously broken    
-console.log("anything")
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("anything")
+    renderGameWindow()
+    renderTeamWindow()
+    
+})
