2d7a456600a103e2cf1968cc8aab2d7d80ed66db
Omar <omar.branez@gmail.com>
2021-08-30 17:04:47 -0400

    render rectangles over current menu selection and clickable area

--- src/index.js
+++ src/index.js
@@ -2,25 +2,7 @@ const gameBackgroundCanvas = document.getElementById("game-background")
 const gameBackgroundContext = gameBackgroundCanvas.getContext("2d")
 const teamBackgroundCanvas = document.getElementById("team-background")
 const teamBackgroundContext = teamBackgroundCanvas.getContext("2d")
-// const BackgroundImage = Image;
-// class ProxyBackground {
-//     constructor(w, h) {
-//         const gameBackground = new BackgroundImage(w, h)
-//         const handler = {
-//             set: function(obj, prop, value) {
-//               if (prop === 'src') {
-//                 console.log('gotcha ' + value);
-//               }
-//               return gameBackground[prop] = value;
-//             },
-//             get: function(target, prop) {
-//               return target[prop];
-//             }
-//           };
-//         return new Proxy(gameBackground, handler)
-//     }
-// }
-// Image = ProxyBackground
+
 
 let currentScreen = "initial" 
 let ongoingBattle = false   
@@ -33,16 +15,13 @@ function renderGameWindow() {
     gameBackgroundCanvas.height = 512
     gameBackgroundCanvas.width = 888
     // const screens = ["title", "creation", "login", "options", "overworld", "battle", "result"]
-    // let gameBackground = new Image()
-    // const BackgroundImage = Image;
-    // let gameBackground = new BackgroundImage()
+
     let gameBackground = new Image()
     
     switch (currentScreen) {
         case "title":
             clearScreen()
             gameBackground.src = "./assets/title-spritesheet.png"
-            // gameBackground = "./assets/title-spritesheet.png"
             spritesheetAnimate(5, 41, 2220, 10250, gameBackground)
             break
         case "initial":
@@ -63,7 +42,10 @@ function renderGameWindow() {
             gameBackground.src = "./assets/nugget-bridge-login.png"
             break
         case "overworld":
-            gameBackground.src = "./assets/nugget-bridge-overworld.png"
+            clearScreen()
+            gameBackground.src = "./assets/route-24-full.png"
+            staticDisplay(gameBackground)
+            console.log("rendering overworld")
             break
         case "battle":
             gameBackground.src = "./assets/nugget-bridge-battle.png"
@@ -114,8 +96,6 @@ function spritesheetAnimate(numColumns, numRows, sheetWidth, sheetHeight, bgImag
 }
 
 function staticDisplay(bgImage) {
-    // clearScreen()
-    // debugger
     bgImage.onload = function() {
         gameBackgroundContext.drawImage(bgImage, 0, 0, 888, 512)
         console.log("i'm displaying the static background!")
@@ -132,7 +112,5 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("anything")
     renderGameWindow()
     renderTeamWindow()
-    // console.log(gameBackground.src)
-
     
 })
--- src/menu.js
+++ src/menu.js
@@ -28,22 +28,54 @@
 // 
 
 // const { clear } = require("console");
+
+let menuState = "main"
+
 const gameButtonCanvas = document.getElementById("menu-buttons")
 const gameButtonCanvasContext = gameButtonCanvas.getContext("2d")
+
 function renderInitialMenu(){
-    // gameButtonCanvas.height = 512
-    // gameButtonCanvas.width = 888
-    const initialMenuButton1 = new Image()
-    initialMenuButton1.src = "./assets/menu-button-new-game.png"
-    const initialMenuButton2 = new Image()
-    initialMenuButton2.src = "./assets/menu-button-continue.png"
+    // let buttons = [newGameButton, continueButton]
+    const newGameButton = new Image()
+    newGameButton.src = "./assets/menu-button-new-game.png"
+    const continueButton = new Image()
+    continueButton.src = "./assets/menu-button-continue.png"
     // const overlayContext = gameCanvas.getContext("2d")
-    initialMenuButton1.onload = function() {
-        gameButtonCanvasContext.drawImage(initialMenuButton1, 400, 150, 150, 100)
-    }
-    initialMenuButton2.onload = function() {
-        gameButtonCanvasContext.drawImage(initialMenuButton2, 400, 250, 150, 100)
-    }
-    console.log("i'm important too!")
+    renderButton(newGameButton, 400, 150, 150, 100)
+    renderButton(continueButton, 400, 250, 150, 100)
+    // gameButtonCanvas.addEventListener()
+    drawSelection()
+    testSelection()
+    
+    console.log("new game menu rendering")
 
 }
+
+function renderButton(gameButton, xLocation, yLocation, width, height){
+    gameButton.onload = function() {
+        gameButtonCanvasContext.drawImage(gameButton, xLocation, yLocation, width, height)
+    }
+}
+// function initialMenuSelect() {
+//     gameButtonCanvasLeft
+// }
+
+function drawSelection(){ //draws rectangle if chosen
+    gameButtonCanvasContext.beginPath();
+    gameButtonCanvasContext.lineWidth = "6";
+    gameButtonCanvasContext.strokeStyle = "red";
+    gameButtonCanvasContext.rect(375, 150, 200, 100);
+    gameButtonCanvasContext.stroke();
+    console.log("drew red rectangle over selection")
+}
+
+function testSelection(){ // see how big the clickable area is, for eventlisteners
+    gameButtonCanvasContext.beginPath();
+    gameButtonCanvasContext.lineWidth = "6";
+    gameButtonCanvasContext.strokeStyle = "blue";
+    gameButtonCanvasContext.rect(400, 270, 150, 60); //clickable area for newgamebutton is 400, 170, 150, 60
+    //continuebutton is 400, 270, 150, 60 AKA add 100 to Y location
+    gameButtonCanvasContext.stroke();
+    console.log("drew blue rectangle over clickable area")
+}
+
